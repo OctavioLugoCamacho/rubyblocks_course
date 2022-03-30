f7c96fa1681ff1b76d5b13d59bba3af63cefebbf
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-30 15:11:14 -0600

    Add the examples of the gotchas in a block

---
+++ block_scoping.rb
@@ -0,0 +1,13 @@
+name = "Octa"
+n = 100
+temp = Math::PI / 4
+
+3.times do |n; temp|
+  name = "Octavio"
+  temp = 98.6
+  puts "#{n} Howdy, #{name} is #{temp}!"
+end
+
+puts name
+puts n
+puts temp
