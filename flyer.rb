06495d2b04ba712b1db5909541ae023b2e75469a
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-30 15:47:11 -0600

    Add exercises of each

--- flyer.rb
+++ flyer.rb
@@ -18,4 +18,18 @@ flyers = []
   flyers << Flyer.new("Flyer #{number}", "flyer#{number}@example.com", number * 1000)
 end
 
-puts flyers
+flyers.each do |flyer|
+  puts "#{flyer.name} - #{flyer.miles_flown} miles"
+end
+
+total = 0
+flyers.each do |flyer|
+  total += flyer.miles_flown
+end
+
+puts "Total miles flown: #{total}"
+
+promotions = { "United" => 1.5, "Delta" => 2.0, "Lufthansa" => 2.5 }
+promotions.each do |airline, multiplier|
+  puts "Earn #{multiplier}x miles by flying #{airline}!"
+end
--- order.rb
+++ order.rb
@@ -16,4 +16,15 @@ orders = []
   orders << Order.new("octavio#{n}@makingdevs.com", n * 10)
 end
 
-puts orders
+puts "Newsletters emails:"
+
+orders.each { |o| puts o.email}
+
+sum = 0
+orders.each { |o| sum += o.total}
+puts "Total sales: $#{sum}"
+
+taxes = { "CO" => 0.02, "MT" => 0.00, "AZ" => 0.04}
+taxes.each do |key, value|
+  puts "#{key}: #{value * 100}%"
+end
