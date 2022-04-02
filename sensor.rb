7335a079a2a05d41a6458f960ba9fb1f30b45d3e
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-01 20:13:09 -0600

    Add the use cases exercises: execute around

--- sensor.rb
+++ sensor.rb
@@ -24,4 +24,34 @@ end
 checking("temperature") { sensor.temperature < 150 }
 checking("level") { sensor.level > 3 }
 
+def h1
+  puts "<h1>"
+  puts yield
+  puts "</h1>"
+end
+
+def h2
+  puts "<h2>"
+  puts yield
+  puts "</h2>"
+end
+
+def tag(name)
+  puts "<#{name}>"
+  puts yield
+  puts "</#{name}>"
+end
+
+h1 { "Breaking News!" }
+h2 { "Massive Ruby Discovered" }
+
+tag(:h1) { "Breaking News!" }
+tag(:h2) { "Massive Ruby Discovered" }
+
+tag(:ul) do
+  tag(:li) { "It sparkles!"}
+  tag(:li) { "It shines!"}
+  tag(:li) { "It mesmerizes!"}
+end
+
 
